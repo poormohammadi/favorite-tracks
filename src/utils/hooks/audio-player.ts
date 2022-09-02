f6f2e327dbafdcaf3423fe63470351d69bc68b2a
Ali Poormohammadi <>
2022-09-02 16:30:46 +0430

    fix: update audio player hook

--- src/utils/hooks/audio-player.ts
+++ src/utils/hooks/audio-player.ts
@@ -1,60 +1,62 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Audio, AVPlaybackStatusSuccess } from "expo-av";
+import { Platform } from "react-native";
 
 type AudioPlayerProps = {
 	audioUrl?: string;
 
 }
 export const useAudioPlayer = ({ audioUrl }: AudioPlayerProps) => {
-	const [sound, setSound] = useState<Audio.Sound>();
 	const [playbackStatus, setPlaybackStatus] = useState<AVPlaybackStatusSuccess>();
 	const [isLoading, setIsLoading] = useState(false);
 
 	let interval: NodeJS.Timer;
+	const audio = useRef(new Audio.Sound());
 
 	const playSound = useCallback(async () => {
-		if (sound) {
-			await sound.playAsync();
-			sound.setOnPlaybackStatusUpdate(setPlaybackStatus as never);
+		if (playbackStatus?.positionMillis / playbackStatus?.durationMillis > 0.99) {
+			audio.current.playFromPositionAsync(0);
+		} else {
+			audio.current.playAsync();
 		}
-	}, [sound]);
+		audio.current.setOnPlaybackStatusUpdate(setPlaybackStatus as never);
+	}, [playbackStatus]);
+
+	const pauseSound = useCallback(async () => {
+		if (audio.current) {
+			await audio.current.pauseAsync();
+		}
+	}, [audio.current]);
 
 	useEffect(() => {
-		if (sound) {
+		if (Platform.OS === "ios") {
 			// There seem to be a bug with expo-av as it doesn't call setOnPlaybackStatusUpdate frequently
 			// So I had to add this function here
 			// Ideally this useEffect should be removed
 			interval = setInterval(() => {
-				sound.getStatusAsync();
-			}, 400);
+				audio.current.getStatusAsync();
+			}, 200);
 		}
-	}, [sound]);
+	}, [audio.current]);
 
 	useEffect(() => {
 		const loadSound = async () => {
 			setIsLoading(true);
-			const { sound } = await Audio.Sound.createAsync({ uri: audioUrl });
-			setSound(sound);
-			setIsLoading(false);
+			audio.current.loadAsync({ uri: audioUrl });
 		};
-
 		loadSound();
+		
+		return () => {
+			clearInterval(interval);
+			audio?.current?.unloadAsync();
+		};
 	}, []);
 
 	const playFromPosition = useCallback((seconds: number) => {
-		if (sound) {
-			sound.playFromPositionAsync(seconds);
+		if (audio.current) {
+			audio.current?.setPositionAsync(seconds);
 		}
-	}, [sound]);
+	}, []);
 
-	useEffect(() => {
-		return sound
-			? () => {
-				clearInterval(interval);
-				sound.unloadAsync();
-			}
-			: undefined;
-	}, [sound]);
-
-	return { playSound, isLoading, playbackStatus, playFromPosition };
+	return { playSound, isLoading, playbackStatus, playFromPosition, pauseSound };
 };
