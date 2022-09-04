7d978a076208ca6f80e3ef679ae530d6bc7f265c
Ali Poormohammadi <>
2022-09-04 04:39:12 +0430

    fix: typography colors

--- src/components/typography/Typography.styles.ts
+++ src/components/typography/Typography.styles.ts
@@ -9,17 +9,14 @@ export const Tiny = styled.Text`
 export const Title = styled(Tiny)`
   font-size: ${(props) => props.theme.fontSize.large};
   line-height: ${(props) => props.theme.normalize(28)}px;
-  color: ${(props) => props.theme.colors.text.prominent};
 `;
 
 export const SubTitle = styled(Tiny)`
   font-size: ${(props) => props.theme.fontSize.normal};
   line-height: ${(props) => props.theme.normalize(24)}px;
-  color: ${(props) => props.theme.colors.text.faded};
 `;
 
 export const Small = styled(Tiny)`
   font-size: ${(props) => props.theme.fontSize.small};
   line-height: ${(props) => props.theme.normalize(20)}px;
-  color: ${(props) => props.theme.colors.text.faded};
 `;
